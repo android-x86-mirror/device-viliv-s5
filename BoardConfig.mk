2643cd55ecb6a9cc03d235b638400d18c2384f88
Owen Kwon <pinebud77@hotmail.com>
2010-07-07 17:27:27 +0900

    added mmx/sse defines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,10 @@
 TARGET_HAS_THIRD_PARTY_APPS := true
 BOARD_WPA_SUPPLICANT_DRIVER := true
 BOARD_USES_TSLIB := true
+BOARD_USES_MMX := true
+BOARD_USES_SSE := true
+BOARD_USES_SSE2 := true
+BOARD_USES_SSE3 := true
 BOARD_DISABLE_PM := true
 TARGET_GLOBAL_CFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
 TARGET_GLOBAL_CPPFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
