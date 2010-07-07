5243f452bc0a4aa34359c7f70476fc189a52d848
Owen Kwon <pinebud77@hotmail.com>
2010-07-07 12:07:21 +0900

    modified CFLAGS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,8 +2,8 @@ TARGET_HAS_THIRD_PARTY_APPS := true
 BOARD_WPA_SUPPLICANT_DRIVER := true
 BOARD_USES_TSLIB := true
 BOARD_DISABLE_PM := true
-TARGET_GLOBAL_CFLAGS += -O2 -march=pentium -msse -msse2 -msse3 -mfpmath=sse
-TARGET_GLOBAL_CPPFLAGS += -O2 -march=pentium -msse -msse2 -msse3 -mfpmath=sse
+TARGET_GLOBAL_CFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
+TARGET_GLOBAL_CPPFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
 
 include $(GENERIC_X86_CONFIG_MK)
 
