db60be859283b67efe5bd18ec7b4617889638345
Owen Kwon <pinebud77@hotmail.com>
2010-07-09 18:49:07 +0900

    added mplayer flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,10 @@ BOARD_USES_SSE := true
 BOARD_USES_SSE2 := true
 BOARD_USES_SSE3 := true
 BOARD_DISABLE_PM := true
+
+#test flag T_T
+BUILD_WITH_MPLAYER := true
+
 TARGET_GLOBAL_CFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
 TARGET_GLOBAL_CPPFLAGS += -O2 -mhard-float -march=i586 -mmmx -msse -msse2 -msse3 -mfpmath=sse
 
