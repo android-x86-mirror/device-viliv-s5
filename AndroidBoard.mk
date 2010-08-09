a2c7625c3be5fdd44b862f98b9e13412eac450ad
Ohkeun Kwon <pinebud@gmail.com>
2010-08-09 22:05:00 +0900

    removed psb building part

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -15,22 +15,5 @@ $(call add-prebuilt-targets,$(TARGET_OUT_ETC), init.s5.sh)
 #copy alsa configurations
 $(call add-prebuilt-targets,$(TARGET_OUT_ETC), asound.conf)
 
-#compile and add psb modules
-#this is not an Android Makefile format :(
-#ToDo : need to find a way to configure the path -_-;;
-KBUILD_OUTPUT := $(CURDIR)/$(TARGET_OUT_INTERMEDIATES)/kernel
-PSB_SRC_DIR := psb-android
-$(LOCAL_PATH)/drm-psb.ko : kernel $(LOCAL_PATH)/psb.ko
-	cp $(TARGET_DEVICE_DIR)/$(PSB_SRC_DIR)/drm-psb.ko $(TARGET_DEVICE_DIR)
-
-
-$(LOCAL_PATH)/psb.ko : kernel
-	$(hide) $(MAKE) -C$(TARGET_DEVICE_DIR)/$(PSB_SRC_DIR) \
-		LINUXDIR=$(KBUILD_OUTPUT) DRM_MODULES=psb
-	cp $(TARGET_DEVICE_DIR)/$(PSB_SRC_DIR)/psb.ko $(TARGET_DEVICE_DIR)
-
-$(call add-prebuilt-targets,$(TARGET_OUT)/lib/modules, drm-psb.ko)
-$(call add-prebuilt-targets,$(TARGET_OUT)/lib/modules, psb.ko)
-
 include $(GENERIC_X86_ANDROID_MK)
 
